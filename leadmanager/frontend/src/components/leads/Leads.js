d6d7d55b22f3981a5336cadc921031183abe16c2
Vadym Romanchuk <vadumo@ukr.net>
2019-06-06 20:32:59 +0300

    button

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,60 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from "react-redux";
+import PropTypes from 'prop-types';
+import { getLeads } from '../../actions/leads';
+
+
+export class Leads extends Component {
+    static propTypes = {
+        leads: PropTypes.array.isRequired
+    };
+
+    componentDidMount() {
+        this.props.getLeads();
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <h2>Leads List</h2>
+                <table className="table table-striped" >
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Date create</th>
+                            <th>Group</th>
+                            <th>Actions</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.leads.map(lead => (
+                            <tr key={lead.id}>
+                                <td>{lead.name}</td>
+                                <td>{lead.created_at}</td>
+                                <td></td>
+                                <td>
+                                    <button className="btn btn-outline-danger">
+                                    Delete</button>
+                                </td>
+                                <td>
+                                    <button className="btn btn-outline-warning">
+                                    Edit</button>
+                                </td>
+                            </tr>
+                        )) }
+                    </tbody>
+                 </table>
+            </Fragment>
+         );
+    }
+}
+
+const mapStateToProps = state => ({
+    leads: state.leads.leads
+});
+
+export default connect(
+    mapStateToProps,
+    { getLeads }
+)(Leads);
