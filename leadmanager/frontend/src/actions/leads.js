80fcfd342758673b2dc162b66ee27b2532806a6e
Vadym Romanchuk <vadumo@ukr.net>
2019-06-07 00:33:04 +0300

    add lead

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 //GET LEADS
 export const getLeads = () => dispatch => {
@@ -27,3 +27,16 @@ export const deleteLead = (id) => dispatch => {
         })
         .catch(err => console.log(err));
 }
+
+// ADD LEAD
+export const addLead = lead => dispatch => {
+    axios
+        .post("/api/leads/", lead)
+        .then(res => {
+            dispatch({
+                type: ADD_LEAD,
+                payload: res.data
+            });
+        })
+        .catch(err => console.log(err));
+};
--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,4 +1,4 @@
-import { GET_LEADS, DELETE_LEAD } from "../actions/types.js";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "../actions/types.js";
 
 const initialState = {
     leads: []
@@ -17,6 +17,11 @@ export default function(state = initialState, action) {
                 leads: state.leads.filter(lead => lead.id !==
                 action.payload)
             }
+        case ADD_LEAD:
+            return {
+                ...state,
+                leads: [...state.leads, action.payload]
+            };
         default:
             return state;
     }
